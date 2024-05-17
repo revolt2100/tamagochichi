846c05dc41fe2839854dba8b8b20d23aa91a43d0
katherine b. <kv.borodini@gmail.com>
2024-05-18 00:53:27 +0300

    icon, bunny state pictures; sleeping and working state shown, no shop — items come from work

--- copium_final.py
+++ copium_final.py
@@ -14,22 +14,17 @@ from pygame.locals import (
 
 os.chdir(os.path.dirname(__file__))
 
-def absent(n, callbacks = []):
+def absent(m, n, callbacks = []):
     start_time = time.time()
     while time.time() - start_time < n:
         global state
-        state = 'ABSENT'
+        state = m
     state = 'RUNNING'
     for c in callbacks: c()
 
-def f_with_timer(f = 2, callbacks = []):
-   threadd = threading.Thread(target=lambda: absent(f, callbacks))
+def f_with_timer(m, n = 2, callbacks = []):
+   threadd = threading.Thread(target=lambda: absent(m, n, callbacks))
    threadd.start()
-
-#def ending():
-   # global state
-   # state = 'END'
-
 
 
 class MyButton:
@@ -87,6 +82,9 @@ class MyBar:
 
 pygame.init()
 
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
 screen_width = 600
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -96,16 +94,23 @@ pygame.mixer.init()
 music = pygame.mixer.music.load('waaaa_povle.ogg')
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(loops = -1)
+
 background = pygame.image.load(image_background)
 pile_of_books = pygame.image.load('pile_of_books.png').convert_alpha()
 very_normal_mushroom = pygame.image.load('very_normal_mushroom.png').convert_alpha()
 communication_device = pygame.image.load('communication_device.png').convert_alpha()
+
 eepy = pygame.image.load('eepy.png').convert_alpha()
 full = pygame.image.load('full.png').convert_alpha()
 fun = pygame.image.load('fun.png').convert_alpha()
 bunny = pygame.image.load('bunny.png').convert_alpha()
+bunny_eeping = pygame.image.load('bunny_eeping.png').convert_alpha()
+bunny_working = pygame.image.load('bunny_working.png').convert_alpha()
+
 bunny_width = bunny.get_width()
 bunny_height = bunny.get_height()
+
+item_shadow = pygame.image.load('item_shadow.png').convert_alpha()
 
 white = (255, 255, 255)
 clock = pygame.time.Clock()
@@ -114,7 +119,7 @@ def work_done():
     #global money_count
     global state_of_items
     #money_count += random.randint(1, 12)
-    item = random.choice([False, pile_of_books, very_normal_mushroom, communication_device])
+    item = random.choice([pile_of_books, very_normal_mushroom, communication_device])
     if item:
         state_of_items = item 
  
@@ -131,10 +136,10 @@ bars = [
 
 feed_button = MyButton('feed.png', 25, 600, [food_bar.increasing])
 play_button = MyButton('play.png', 175, 605, [fun_bar.increasing])
-sleep_button = MyButton('sleep.png', 305, 605, [sleep_bar.increasing, lambda: f_with_timer(10)])
-work_button = MyButton('work.png', 445, 605, [lambda: f_with_timer(5, [work_done])])
+sleep_button = MyButton('sleep.png', 305, 605, [sleep_bar.increasing, lambda: f_with_timer('ABSENT_s', 10)])
+work_button = MyButton('work.png', 445, 605, [lambda: f_with_timer('ABSENT_w', 5, [work_done])])
 settings_button = MyButton('settings.png', 20, 20)
-shop_button = MyButton('shop.png', 20, 92)
+#shop_button = MyButton('shop.png', 20, 92)
 
 
 buttons = [
@@ -143,14 +148,15 @@ buttons = [
     sleep_button,
     work_button,
     settings_button,
-    shop_button
+    #shop_button
 ]
 
 state = 'RUNNING'
 running = True
 state_of_items = None
+
 while running:
-    print(state_of_items)
+    print(state)
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
@@ -160,7 +166,7 @@ while running:
                 running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for button in buttons:
-                if state == 'ABSENT':
+                if state == 'ABSENT_w' or state == 'ABSENT_s':
                     continue
                 else:
                     if button.clicked():
@@ -172,18 +178,24 @@ while running:
     screen.blit(background, (0, 0))
     if state == 'RUNNING':
         screen.blit(bunny, (screen_width / 2 - bunny_width / 2, screen_height / 2 - bunny_height / 2))
+    elif state =='ABSENT_s':
+        screen.blit(bunny_eeping, (screen_width / 2 - bunny_eeping.get_width() / 2, screen_height / 2 - bunny_eeping.get_height() / 2 + 110))
+    elif state =='ABSENT_w':
+        screen.blit(bunny_working, (screen_width / 2 - bunny_working.get_width() / 2, screen_height / 2 - bunny_working.get_height() / 2 + 110))
     screen.blit(full, (383, 23))
     screen.blit(eepy, (381, 52))
     screen.blit(fun, (375, 81))
     if state_of_items != None:
-        screen.blit(state_of_items, (0, 0))
-    pw.update(events)
+        item_height = state_of_items.get_height()
+        screen.blit(item_shadow, (454, 530))
+        screen.blit(state_of_items, (454, 550 - item_height))
     for button in buttons:
         screen.blit(button.image, (button.x, button.y))
     for bar in bars:
         current_width = (bar.current_bar / bar.max_bar) * bar.width    
         pygame.draw.rect(screen, white, (bar.x, bar.y, bar.width, bar.height))
         pygame.draw.rect(screen, bar.colour, (bar.x, bar.y, current_width, bar.height))    
+    pw.update(events)
     pygame.display.update()
     clock.tick(60)
 
